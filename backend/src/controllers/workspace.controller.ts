2bab00cee9a9e8089caa58911a8c5b9b7644890b
madhur-banger <madhur.cloudevops@gmail.com>
2025-12-10 09:25:12 +0530

    feat: Add Swagger Docsw

--- backend/src/controllers/workspace.controller.ts
+++ backend/src/controllers/workspace.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../middlewares/asyncHandler.middleware";
 import { createWorkspaceSchema } from "../validation/workspace.validation";
+import { createWorkspaceService } from "../services/workspace.service";
+import { HTTPSTATUS } from "../config/http.config";
 
 
 
@@ -9,6 +11,11 @@ export const createWorkspaceController = asyncHandler(
         const body = createWorkspaceSchema.parse(req.body);
 
         const userId = req.user?._id;
-        // const workspace = await 
+        const {workspace} = await createWorkspaceService(userId, body);
+
+        return res.status(HTTPSTATUS.CREATED).json({
+            message: "Workspace created successfully",
+            workspace,
+        });
     }
-)
+);
