eef1e05a7206ce12a83851e735016ed945787da7
madhur-banger <madhur.cloudevops@gmail.com>
2025-12-16 11:37:57 +0530

    feat: Add Task Validatrion

---
+++ backend/src/validation/task.validation.ts
@@ -0,0 +1,50 @@
+import { z } from "zod";
+import { TaskPriorityEnum, TaskStatusEnum } from "../enums/task.enum";
+
+export const titleSchema = z.string().trim().min(1).max(255);
+export const descriptionSchema = z.string().trim().optional();
+
+export const assignedToSchema = z.string().trim().min(1).nullable().optional();
+
+export const prioritySchema = z.enum(
+    Object.values(TaskPriorityEnum) as [string, ...string[]]
+);
+
+export const statusSchema = z.enum(
+    Object.values(TaskStatusEnum) as [string, ...string[]]
+);
+
+export const dueDateSchema = z
+    .string()
+    .trim()
+    .optional()
+    .refine(
+        (val) => {
+            return !val || !isNaN(Date.parse(val));
+        },
+        {
+            message: "Inval;id date Format. Please provide a valid date string"
+        }
+    )
+
+export const taskIdSchema = z.string().trim().min(1);
+
+export const createTasksSchema = z.object({
+    title: titleSchema,
+    description: descriptionSchema,
+    priority: prioritySchema,
+    status: statusSchema,
+    assignedTo: assignedToSchema,
+    dueDate: dueDateSchema,
+});
+
+
+export const updateTaskSchema = z.object({
+    title: titleSchema,
+    description: descriptionSchema,
+    priority: prioritySchema,
+    status: statusSchema,
+    assignedTo: assignedToSchema,
+    dueDate: dueDateSchema,
+  });
+  
