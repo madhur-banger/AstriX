8bc813cbcce43d62328b0ae1376fadc17aa62398
madhur-banger <madhur.cloudevops@gmail.com>
2025-12-16 12:46:14 +0530

    fix: Error Midlleware

--- backend/src/middlewares/asyncHandler.middleware.ts
+++ backend/src/middlewares/asyncHandler.middleware.ts
@@ -1,17 +1,17 @@
-import { NextFunction,  Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
-type AsyncControllerType = (
-    req: Request,
-    res: Response,
-    next: NextFunction
+type AyncControllerType = (
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => Promise<any>;
 
-export const asyncHandler = 
-    (controller: AsyncControllerType):AsyncControllerType =>
-        async (req,res, next) => {
-            try {
-                await controller(req, res, next);
-            } catch (error) {
-                next(error);
-            };
-        };
+export const asyncHandler =
+  (controller: AyncControllerType): AyncControllerType =>
+  async (req, res, next) => {
+    try {
+      await controller(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
