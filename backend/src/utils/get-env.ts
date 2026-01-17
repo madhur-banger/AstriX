3bc7548d1a3abe8f8fa8fe5daeae20c4c6ab9998
madhur-banger <madhur.cloudevops@gmail.com>
2026-01-18 00:08:39 +0530

    feat(backend): add get-env utility

---
+++ backend/src/utils/get-env.ts
@@ -0,0 +1,12 @@
+export const getEnv = <T extends string>(key: string, defaultValue?: T): T => {
+  const value = process.env[key];
+
+  if (value === undefined) {
+    if (defaultValue !== undefined) {
+      return defaultValue;
+    }
+    throw new Error(`Environment variable ${key} is not set`);
+  }
+
+  return value as T;
+};
