2efcbc6a91e3fcae5f0f6eb01ecfdef735efb5b8
madhur-banger <madhur.cloudevops@gmail.com>
2026-02-14 11:45:14 +0530

    feat(backend): wire workspaces routes

---
+++ backend/src/routes/workspace.routes.ts
@@ -0,0 +1,39 @@
+import { Router } from "express";
+import {
+  changeWorkspaceMemberRoleController,
+  createWorkspaceController,
+  deleteWorkspaceByIdController,
+  getAllWorkspacesUserIsMemberController,
+  getWorkspaceAnalyticsController,
+  getWorkspaceByIdController,
+  getWorkspaceMembersController,
+  leaveWorkspaceController,
+  removeWorkspaceMemberController,
+  resetWorkspaceInviteCodeController,
+  updateWorkspaceByIdController,
+} from "../controllers/workspace.controller";
+
+const workspaceRoutes = Router();
+
+workspaceRoutes.post("/create/new", createWorkspaceController);
+workspaceRoutes.put("/update/:id", updateWorkspaceByIdController);
+
+workspaceRoutes.put(
+  "/change/member/role/:id",
+  changeWorkspaceMemberRoleController
+);
+
+workspaceRoutes.delete("/delete/:id", deleteWorkspaceByIdController);
+
+workspaceRoutes.delete("/:id/member/:userId", removeWorkspaceMemberController);
+workspaceRoutes.post("/:id/leave", leaveWorkspaceController);
+workspaceRoutes.post("/:id/invite/reset", resetWorkspaceInviteCodeController);
+
+workspaceRoutes.get("/all", getAllWorkspacesUserIsMemberController);
+
+workspaceRoutes.get("/members/:id", getWorkspaceMembersController);
+workspaceRoutes.get("/analytics/:id", getWorkspaceAnalyticsController);
+
+workspaceRoutes.get("/:id", getWorkspaceByIdController);
+
+export default workspaceRoutes;
