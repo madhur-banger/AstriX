033ed6bf4b2e78cf3fe4eaec0a568e507588fe55
madhur-banger <madhur.cloudevops@gmail.com>
2026-02-16 05:40:21 +0530

    feat(backend): wire task routes

---
+++ backend/src/routes/task.route.ts
@@ -0,0 +1,31 @@
+import { Router } from "express";
+import {
+  createTaskController,
+  deleteTaskController,
+  getAllTasksController,
+  getTaskByIdController,
+  updateTaskController,
+} from "../controllers/task.controller";
+
+const taskRoutes = Router();
+
+taskRoutes.post(
+  "/project/:projectId/workspace/:workspaceId/create",
+  createTaskController
+);
+
+taskRoutes.put(
+  "/:id/project/:projectId/workspace/:workspaceId/update",
+  updateTaskController
+);
+
+taskRoutes.get("/workspace/:workspaceId/all", getAllTasksController);
+
+taskRoutes.delete("/:id/workspace/:workspaceId/delete", deleteTaskController);
+
+taskRoutes.get(
+  "/:id/project/:projectId/workspace/:workspaceId",
+  getTaskByIdController
+);
+
+export default taskRoutes;
