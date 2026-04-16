1caed9c2cfe23acca52c21f629d67a528e712e36
madhur-banger <madhur.cloudevops@gmail.com>
2026-04-17 02:28:20 +0530

    feat(client): add auth context provider

---
+++ client/src/context/auth-provider.tsx
@@ -0,0 +1,108 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+} from "react";
+import useWorkspaceId from "@/hooks/use-workspace-id";
+import useAuth from "@/hooks/api/use-auth";
+import { UserType, WorkspaceType } from "@/types/api.type";
+import useGetWorkspaceQuery from "@/hooks/api/use-get-workspace";
+import { useNavigate } from "react-router-dom";
+import usePermissions from "@/hooks/use-permissions";
+import { PermissionType } from "@/constant";
+
+// Define the context shape
+type AuthContextType = {
+  user?: UserType;
+  workspace?: WorkspaceType;
+  hasPermission: (permission: PermissionType) => boolean;
+  error: any;
+  isLoading: boolean;
+  isFetching: boolean;
+  workspaceLoading: boolean;
+  refetchAuth: () => void;
+  refetchWorkspace: () => void;
+};
+
+const AuthContext = createContext<AuthContextType | undefined>(undefined);
+
+export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const navigate = useNavigate();
+  const workspaceId = useWorkspaceId();
+
+  const {
+    data: authData,
+    error: authError,
+    isLoading,
+    isFetching,
+    refetch: refetchAuth,
+  } = useAuth();
+  const user = authData?.user;
+
+  const {
+    data: workspaceData,
+    isLoading: workspaceLoading,
+    error: workspaceError,
+    refetch: refetchWorkspace,
+  } = useGetWorkspaceQuery(workspaceId);
+
+  const workspace = workspaceData?.workspace;
+
+  useEffect(() => {
+    if (workspaceError) {
+      if (workspaceError?.errorCode === "ACCESS_UNAUTHORIZED") {
+        navigate("/unauthorized"); // Redirect if the user is not a member of the workspace
+      }
+    }
+  }, [navigate, workspaceError]);
+
+  const permissions = usePermissions(user, workspace);
+
+  const hasPermission = useCallback(
+    (permission: PermissionType): boolean => permissions.includes(permission),
+    [permissions]
+  );
+
+  const error = authError || workspaceError;
+
+  const value = useMemo(
+    () => ({
+      user,
+      workspace,
+      hasPermission,
+      error,
+      isLoading,
+      isFetching,
+      workspaceLoading,
+      refetchAuth,
+      refetchWorkspace,
+    }),
+    [
+      user,
+      workspace,
+      hasPermission,
+      error,
+      isLoading,
+      isFetching,
+      workspaceLoading,
+      refetchAuth,
+      refetchWorkspace,
+    ]
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+// eslint-disable-next-line react-refresh/only-export-components
+export const useAuthContext = () => {
+  const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error("useCurrentUserContext must be used within a AuthProvider");
+  }
+  return context;
+};
